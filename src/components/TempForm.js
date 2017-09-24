288bf7e66c88d58e53cc4de61b6e7002c2f5b2f8
kaileighrose <kaileighrose@gmail.com>
2017-09-23 20:20:06 -0400

    build preferences form and move properties into state field of constructor method in TempForm class

--- src/components/TempForm.js
+++ src/components/TempForm.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react';
 
 class TempForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _min: '',
+      _max: '',
+      _risk: ''
+    };
+  }
+
   render() {
     return (
-
+      <form className="App-form" onSubmit={this._handleSubmit.bind(this)}>
+        <div className="app-form-fields">
+          <label>Minimum Temperature: </label>
+          <input type="number" placeholder="Min" ref={c => this._min = c} />
+          <label>  Maximum Temperature: </label>
+          <input type="number" placeholder="Max" ref={c => this._max = c} />
+          <label>  Maximum Chance of Rain: </label>
+          <input type="number" placeholder="Max" ref={c => this._risk = c} />
+        </div>
+        <div className="app-form-actions">
+          <button type="submit">Check Weather</button>
+        </div>
+      </form>
     );
   }
 
   _handleSubmit(event) {
     event.preventDefault();
-
+    console.log(_min, _max, _risk);
   }
 
-  this._min.value = '';
-  this._max.value = '';
-  this._risk.value = '';
+
 
 }
